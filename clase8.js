9a379cbdb5df022e52ddf6938b825b92d0f11336
Bramar95 <bramar2195@gmai.com>
2021-11-10 16:34:38 -0300

    subi la clase 8

---
+++ clase8.js
@@ -0,0 +1,96 @@
+/*METODOLOGIAS DE DESARROLLO DE SOFTWARE
+Metodologias de cascada o secuencial: antes de las metodologías ágiles
+
+Modelo secuencial de desarrollo de software
+
+System requirements
+    Software requirements
+        Analysis
+            Program design
+                Coding
+                    Testing
+                        Operations
+-Origen de la automatizacion de tareas: Revolucion industrial 1760, modelo secuencial de pasos
+-En 1970 Winston Royce postula un documento en el que propone evidenciar las deficiencias a mejorar del modelo de cascada
+-En 1985 el Department of defense adapta sin tomar en cuenta las advertencias el documento de Royce
+-Se empieza a implementar como requisito reglamentario que todas las empresas y organizaciones desarrolladoras de software tomen el modelo de cascada
+
+Funciona bien en casos:
+-En situaciones que conocemos bien las variables desde el inicio del proyecto
+-En situaciones en las que necesitamos calcular detalle los tiempos y los costos
+
+Debilidades:
+-Deja fuera de la faase de desarrollo al usuario; el usuario no ve el producto hasta estar terminado
+-No funciona bien ante los cambios constantes y cambios de requirimientos
+-Es necesario conocer todos los detalles antes de empezar a trabajar
+-Si una etapa se atrasa se necesita terminarla para avanzar con la siguiente; se traslada el retraso
+*/
+
+/*Metodologias agiles: SCRUM, Extreme programming, FDD, Crystal
+
+-En 1994 se publica el reporte del caos: el 84% de los proyectos iniciados bajo la metodologia secuencial no finalizaban o cumplian con el tiempo y los costos
+
+-Surge el manifiesto agil: un acuerdo para promover las metodologias que ayuden desarrollar software de manera optima
+-Promover la agilidad dentro de todos los aspectos del desarrollo
+-Proceso de desarrollo que evoluciona a su mejor version
+-Se conocen mas las necesidades del cliente
+
+Valores
+-Se valora mas:
+Individuos e interacciones
+Software funcional
+Colaboracion con el cliente
+Respuesta al cambio
+
+-Se valora menos:
+Procesos y herramientas
+Documentacion compleja
+Apego a contratos
+Seguimiento de un plan
+
+Se caracteriza por:
+-Foco en satisfacer al cliente: presente en todo el ciclo de desarrollo
+-Software como medida principal de desarrollo: en cada ciclo se entrega una version incremental o mejor del proyecto, solo lo que esta terminado
+-Mejora constante: reflexion regular del trabajo hecho, de los procesos implementados y de la experiencia del equipo
+-Equipos autoorganizados: los que administran el proyecto deben generar un ambiente productivo y de buena comunicacion. La ejecucion queda a cargo del equipo de desarrollo 
+-El cambio es bienvenido: los requisitos pueden cambiar por devoluviones del cliente o por situaciones externas, incluso en etapas avanzadas
+-Simplicidad: mantener el resultado y minimizar el esfuerzo del equipo
+*/
+
+/*
+Scrum: roles de equipo
+Product owner; dueño del producto: 
+-representa la voz del cliente
+-toma las decisiones finales del producto
+-se asegura que el equipo trabaje de manera adecuada bajo enfoque comercial
+-escribe historias de usuario, las prioriza y las pone en el product backlog
+
+
+Scrum Master; guia del equipo de trabajo, facilitador: 
+-comprueba que cada miembro use sus habilidades unicas en pos del proyecto
+-responsable de contrololar los tiempos, la reuniones, y el proceso en general
+-ayuda a evitar obstaculos
+-proteccion del equipo ante distracciones
+
+Equipo de desarrollo o de trabajo:
+-tienen la responsabilidad 
+-son horizontales; las decisiones se toman por consenso
+-son autoorganizados y versatiles para cumplir todas las tareas asignadas
+
+Stakeholder; accionistas:
+-personas que utilizaran o seran afectadas por producto o servicio
+
+
+Flujo de trbajo
+Product backlog:
+-conjunto de todos los requisitos del proyecto
+-contiene descripciones genericas de funcionalidades deseables
+
+Sprint backlog: 
+-conjunto de requisitos a desarrollar en el sprint actual
+-la lista de tareas es gestionada por equipo de desarrollo
+-la duracion de cada sprint(periodo en el que se lleva a cabo el proyecto) es definifa por el equipo
+-suelen durar de 2 a 4 semanas
+
+
+*/
